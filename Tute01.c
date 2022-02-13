c878b443b2f2590d6ed92ec1adfbf907986eb7bb
Dharmadasa.P.S.K <99175624+IT21311086@users.noreply.github.com>
2022-02-13 09:02:02 +0000

    c coding

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,23 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+  {
+	int mark1, mark2,total,avg;
+	
+	printf("Enter 1st subject mark : ");
+	scanf("%d",&mark1);
+	
+	printf("Enter 2nd subject mark :  ");
+	scanf("%d",&mark2);
+	
+	total = mark1 + mark2 ;
+	avg   = total/2 ; 
+	
+	printf("Avarage mark is : %d",avg);
+	
+	
+
   return 0;
 }
 
